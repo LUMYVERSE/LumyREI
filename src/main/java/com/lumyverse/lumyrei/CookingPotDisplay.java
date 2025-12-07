335fbdaa357801056f0e6501396efcfd17fdf5ef
LUMYVERSE <248309030+LUMYVERSE@users.noreply.github.com>
2025-12-07 19:16:30 +0100

    Cleanup CookingPotDisplay comments

--- src/main/java/com/lumyverse/lumyrei/CookingPotDisplay.java
+++ src/main/java/com/lumyverse/lumyrei/CookingPotDisplay.java
@@ -10,14 +10,10 @@ import java.util.Collections;
 
 public class CookingPotDisplay extends BasicDisplay {
 
-    // Cambia il tipo nel costruttore da CookingPotShapelessRecipe a ? extends CookingPotRecipeBase
     public CookingPotDisplay(RecipeEntry<? extends CookingPotRecipeBase> recipe) {
         super(
-                // Input: getIngredients() è definito in CookingPotRecipeBase (ereditato da Recipe)
-                // Questo funzionerà sia per liste semplici che per griglie (se la ricetta shaped riempie gli spazi vuoti)
-                EntryIngredients.ofIngredients(recipe.value().getIngredients()),
 
-                // Output: getResult() è il getter Java per la proprietà "val result" di Kotlin definita nella Base
+                EntryIngredients.ofIngredients(recipe.value().getIngredients()),
                 Collections.singletonList(EntryIngredients.of(recipe.value().getResult()))
         );
     }
